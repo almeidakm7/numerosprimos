7f1543979862d020026c4bfe5b369dc7c3da8d37
Kaue Mello <almeida.km7@gmail.com>
2023-06-21 22:26:32 -0300

    Codigo para identificar um número primo

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Digite um número inteiro maior que 1: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if(numero <= 1)
+        {
+            Console.WriteLine("O número deve ser maior que 1.");
+        }
+        else
+        {
+            bool ehPrimo = true;
+
+                for(int i = 2; i <= numero / 2; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        ehPrimo = false;
+                        break;
+                    }
+                }
+            
+                if (ehPrimo)
+                {
+                    Console.WriteLine("O número " + numero + " é primo.");
+                }
+                else
+                {
+                    Console.WriteLine("O número " + numero + " não é primo.");
+                }
+        }
+    }
+}
